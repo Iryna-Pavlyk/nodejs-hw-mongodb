1addfd7ddbf02edc4db9ef090c3a27ce8c53a0db
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-06-16 01:54:25 +0300

    Create contacts.js

---
+++ src/db/models/contacts.js
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  isFavourite: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  contactType: {
+    type: String,
+    required: true,
+    enam: ['personal', 'work', 'home'],
+    default: 'personal',
+  },
+  timestamps: true,
+});
+
+export const contactsCollection = model('contacts', contactsSchema);
